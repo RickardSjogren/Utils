463c6b18acefac6d4ed4adc4755f0bf93523ef10
RickardSjogren <r.sjogren89@gmail.com>
2016-06-02 15:06:04 +0200

    Added special block version of the fancy heatmap.

--- plotting/fancy_correlation_map.py
+++ plotting/fancy_correlation_map.py
@@ -18,12 +18,25 @@ import scipy.cluster.hierarchy as sp_hierarchy
 import numpy as np
 import matplotlib.pyplot as plt
 import platform
-from matplotlib import colors, ticker, patches, gridspec
+from matplotlib import colors, ticker, patches, gridspec, font_manager, textpath
+
+# Colors from http://colorbrewer2.org/
+BREWER_COLORS = [
+    [0.8941176470588236, 0.10196078431372549, 0.10980392156862745],
+    [0.21568627450980393, 0.49411764705882355, 0.7215686274509804],
+    [0.30196078431372547, 0.6862745098039216, 0.2901960784313726],
+    [0.596078431372549, 0.3058823529411765, 0.6392156862745098],
+    [1.0, 0.4980392156862745, 0.0],
+    [1.0, 1.0, 0.2],
+    [0.6509803921568628, 0.33725490196078434, 0.1568627450980392],
+    [0.9686274509803922, 0.5058823529411764, 0.7490196078431373],
+    [0.6, 0.6, 0.058823529411764705]
+]
 
 __author__ = 'Rickard Sjögren'
 __copyright__ = 'Copyright (C) 2016 Rickard Sjögren'
 __licence__ = 'MIT'
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 
 def correlation_matrix(*data, on_columns=True):
@@ -127,7 +140,7 @@ def make_fancy_heatmap(data, significance, labels, alpha=.05,
         f.set_size_inches(.35 * m, .35 * n)
     else:
         f = None
-
+    ax.grid(which='minor', linestyle='-', color='gray', zorder=0)
     is_square = n == m
 
     if not is_square:                
@@ -176,8 +189,10 @@ def make_fancy_heatmap(data, significance, labels, alpha=.05,
             
             # Draw cross if not statistically significant.
             if significance[row, col] > alpha and mark_insignificant:
-                ax.plot([x + .2, x+.8], [y + .2, y+.8], 'k-', linewidth=1)
-                ax.plot([x + .2, x+.8], [y+.8, y + .2], 'k-', linewidth=1)
+                ax.plot([x + .2, x+.8], [y + .2, y+.8], 'k-',
+                        linewidth=1, zorder=3)
+                ax.plot([x + .2, x+.8], [y+.8, y + .2], 'k-',
+                        linewidth=1, zorder=3)
                 
             # Draw square if statistically significant.
             if significance[row, col] < alpha and mark_significant:
@@ -197,12 +212,12 @@ def make_fancy_heatmap(data, significance, labels, alpha=.05,
         ax.xaxis.tick_top()
     elif xlabel_loc =='bottom':
         ax.xaxis.tick_bottom()
-    
-    if (not (draw_numbers and is_square)) or ylabel_loc == 'right':
+
+    if  ylabel_loc == 'right':
         ax.yaxis.tick_right()
     elif ylabel_loc == 'left':
         ax.yaxis.tick_left()
-    
+
     ax.set_xticklabels(x_labels, rotation=90 if is_square else 270)
     ax.set_yticklabels(y_labels)
     
@@ -226,10 +241,13 @@ def make_fancy_heatmap(data, significance, labels, alpha=.05,
     all_ticks += ax.xaxis.get_minor_ticks() + ax.yaxis.get_minor_ticks()
     for tic in all_ticks:
         tic.tick1On = tic.tick2On = False
-    
-    ax.grid(which='minor', linestyle='-', color='gray')
+
     ax.invert_yaxis()
-    ax.set_aspect('equal', adjustable='box')                        
+
+    try:
+        ax.set_aspect('equal', adjustable='box')
+    except ValueError:
+        pass
     
     return f, ax
     
@@ -281,7 +299,7 @@ def make_fancy_clustermap(data, significance, labels,
     column_linkage = sp_hierarchy.linkage(column_dist, method=link_method)
     row_linkage = sp_hierarchy.linkage(row_dist, method=link_method)
 
-    f = plt.figure()
+    f = plt.figure(dpi=600)
     gs = gridspec.GridSpec(6, 6)
     axes = np.array([
         [plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1:])],
@@ -327,7 +345,104 @@ def make_fancy_clustermap(data, significance, labels,
     f.subplots_adjust(hspace=0, wspace=0)
     return f, axes
 
-    
+
+def fancy_heatmap_with_blocks(data, significance, labels,
+                              right_labels, lines, class_ids, **kwargs):
+    """ Make a heatmap with extra block division drawn out.
+
+    Assumes a two-block correlation matrix.
+
+    Parameters
+    ----------
+    data : array_like
+        n x m-matrix of data-values
+    significance : array_like
+        n x m-matrix of data p-values
+    labels : Sequence, tuple[Sequence]
+        Sequence of labels to use. If n != m, tuple of sequences with labels.
+    right_labels : list[str]
+        Sequence of block labels drawn on right hand side of plots.
+    lines : list[int]
+        Sequence of rows between each divisor line.
+    class_ids : list[str]
+        Sequence of class ids to determine color of left ticks.
+    **kwargs
+        Keyword arguments passed to :func:`make_fancy_heatmap`
+
+    Returns
+    -------
+    matplotlib.pyplot.Figure
+    matplotlib.pyplot.Axes
+    """
+    font = font_manager.FontProperties()
+    font.set_family('monospace')
+    kwargs['draw_numbers'] = False
+    kwargs['ylabel_loc'] = 'left'
+    m = max(data.shape)
+    n = min(data.shape)
+    blocks = labels[np.argmax(data.shape)]
+    line_kwargs = {
+        'c': 'black', 'linewidth': 2
+    }
+    current_run = 0
+    lines = lines.copy()
+    cum_lines = lines.pop(0)
+
+    # Assign unique color to each block.
+    label_colors = dict(zip(np.unique(class_ids), BREWER_COLORS))
+
+    # Draw heatmap.
+    f, ax = make_fancy_heatmap(data, significance, labels, **kwargs)
+
+    for i, (blocks, right_label) in enumerate(zip(blocks, right_labels)):
+        try:
+            next = right_labels[i + 1]
+        except IndexError:
+            next = None
+
+        if next == right_label  and lines[0] > 1:
+            current_run += 1
+        else:
+            x = min(data.shape)
+            y = i + .5 - current_run / 2
+
+            # If in a run of labels, flip text to vertical.
+            if current_run > 0:
+                w = textpath.TextPath((0, 0), right_label).get_extents(
+                    transform=ax.transData.inverted()).width
+                ax.annotate(right_label, (x, y), xytext=(x, .2 + y + w),
+                            rotation=270, font_properties=font)
+            else:
+                # Label is horizontal.
+                ax.annotate(right_label, (x, .2 + y), font_properties=font)
+            current_run = 0
+
+        # Draw division lines.
+        if i == cum_lines:
+            ax.axhline(i, zorder=3, **line_kwargs)
+            try:
+                cum_lines += lines.pop(0)
+            except IndexError:
+                cum_lines = None
+
+    # Change font and colors of tick-labels.
+    for label, class_id in zip(ax.yaxis.get_ticklabels(), class_ids):
+        label.set_font_properties(font)
+        try:
+            label.set_color(label_colors[class_id])
+        except KeyError:
+            pass
+    for label in ax.xaxis.get_ticklabels():
+        label.set_font_properties(font)
+
+    ax.set_xlim(0, min(data.shape))
+    ax.set_ylim(0, max(data.shape))
+
+    ax.invert_yaxis()
+
+    return f, ax
+
+
 if __name__ == '__main__':
     import argparse
     import pandas as pd
@@ -371,6 +486,25 @@ if __name__ == '__main__':
                         help='optional plot title')
     parser.add_argument('--cluster', default=False, action='store_true',
                         help='If this flag is set, ')
+    parser.add_argument('--image_format', type=str, default='png',
+                        help=('image output format(s). If more '
+                              'than one format is wanted, enter them '
+                              'separated by commas. Example png,svg,tif'))
+    parser.add_argument('--block_labels', type=str, default='',
+                        help=('Row block labels drawn at right hand side of'
+                              ' plot. Add block label for each block separated '
+                              'by commas. '
+                              'Example: label1,label2,label3'))
+    parser.add_argument('--class_ids', type=str, default='',
+                        help='Row class id:s, determines color of left hand '
+                             'side labels. Add class label for each row '
+                             'separated by commas. Example: a,a,b,2,2')
+    parser.add_argument('--block_lines', type=str, default='',
+                        help=('Steps between lines drawn between rows. '
+                              'Each number is interpreted as the steps since '
+                              'the last line. Example: 3,3,2,1'))
+    parser.add_argument('--save_values', default=False, action='store_true',
+                        help=('If set, save csv with correlations and p.'))
     
     args = parser.parse_args()
     
@@ -395,13 +529,19 @@ if __name__ == '__main__':
         labels = [d.columns if not args.use_rows else d.index for d in data]
         index, columns = labels
         m, n = corr.shape
-        
+
+    kwargs = dict()
     if args.cluster:
         if not (args.draw_numbers or len(data) == 2):
             msg = ('Clustermap requires two datasets or that numbers'
                     ' are drawn. It looks horrible otherwise.')
             sys.exit(msg)
         plotter = make_fancy_clustermap
+    elif args.block_labels:
+        plotter = fancy_heatmap_with_blocks
+        kwargs['lines'] = [int(i) for i in args.block_lines.split(',')]
+        kwargs['right_labels'] = args.block_labels.split(',')
+        kwargs['class_ids'] = args.class_ids.split(',')
     else:
         plotter = make_fancy_heatmap
     
@@ -409,7 +549,7 @@ if __name__ == '__main__':
         corr, p, labels, alpha=args.alpha, draw_numbers=args.draw_numbers,
         mark_significant=args.mark_significant,
         mark_insignificant=args.mark_insignificant,
-        title=args.figure_title
+        title=args.figure_title, **kwargs
     )
     
     if len(data) == 1 and not args.o:
@@ -417,8 +557,12 @@ if __name__ == '__main__':
     elif len(data) == 2 and not args.o:
         filename = '_'.join(f[:-4] for f in args.csvfile)  + '_correlation'
     else:
-        filename = args.o
-        
-    f.savefig(filename + '.png', dpi=600, bbox_inches='tight')
-    pd.DataFrame(corr, index=index, columns=columns).to_csv(filename + '.csv')
-    pd.DataFrame(p, index=index, columns=columns).to_csv(filename + '_pvalues.csv')
+        filename = args.o.rsplit('.', 1)[0]
+
+    for image_format in args.image_format.split(','):
+        img_name = '.'.join([filename, image_format])
+        f.savefig(img_name, format=image_format, dpi=600, bbox_inches='tight')
+
+    if args.save_values:
+        pd.DataFrame(corr, index=index, columns=columns).to_csv(filename + '.csv')
+        pd.DataFrame(p, index=index, columns=columns).to_csv(filename + '_pvalues.csv')
