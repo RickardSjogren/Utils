27bf3c93365c63dc98bf088da167833171bb7fea
RickardSjogren <r.sjogren89@gmail.com>
2016-06-17 09:23:47 +0200

    Added key-picking and save-dialog to text-picker.

--- text_picker.py
+++ text_picker.py
@@ -28,37 +28,75 @@ class TextPickerWidget(QtGui.QWidget):
 
     def __init__(self, data, *args, **kwargs):
         super(TextPickerWidget, self).__init__(*args, **kwargs)
-        self.data = data
+        self.data = {str(key): value for key, value in data.items()}
+        self.has_saved = False
+        self.list_view = None
+        self.list_model = None
+        self.text_window = None
+        self.save_button = None
+        self.close_button = None
         self.current_selection = 0
+        self.keys = list(self.data.keys())
+        self.init_ui()
+        self.set_selected(0)
 
+    def init_ui(self):
+        self.setWindowTitle('Filter texts.')
         layout = QtGui.QHBoxLayout()
         self.list_view = QtGui.QListView(self)
-
-        self.keys = list(data.keys())
         self.list_model = TextModel(self.keys)
         self.list_view.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
         self.list_view.setModel(self.list_model)
         self.list_view.selectionModel().selectionChanged.connect(
             self.list_changed
         )
-
         self.text_window = QtGui.QTextEdit(self)
         self.text_window.setReadOnly(True)
-
-        self.set_selected(0)
-
         layout.addWidget(self.list_view)
         layout.addWidget(self.text_window)
-
         self.setFocusPolicy(QtCore.Qt.NoFocus)
-
         self.save_button = QtGui.QPushButton('Save')
         self.save_button.clicked.connect(self.save)
-        layout.addWidget(self.save_button)
+        self.close_button = QtGui.QPushButton('Close')
+        self.close_button.clicked.connect(self.close)
+
+        self.base_string = '{{}} / {}'.format(len(self.data))
+        self.label = QtGui.QLabel()
+        sub_layout = QtGui.QVBoxLayout()
+        sub_layout.addWidget(self.label)
+        sub_layout.addStretch()
+        sub_layout.addWidget(self.save_button)
+        sub_layout.addWidget(self.close_button)
+        layout.addLayout(sub_layout)
         self.setLayout(layout)
 
-    def save(self):
+    def close(self):
+        if not self.has_saved:
+            question = ('You have not saved.\n\nWould you '
+                        'like to save before closing?')
+            answer = QtGui.QMessageBox().question(
+                self, 'Save?', question,
+                QtGui.QMessageBox.Yes | QtGui.QMessageBox.No |
+                QtGui.QMessageBox.Abort)
 
+            if answer == QtGui.QMessageBox.Yes:
+                did_save = self.save()
+                if not did_save:
+                    return
+
+            elif answer == QtGui.QMessageBox.Abort:
+                return
+
+        super(TextPickerWidget, self).close()
+
+    def save(self):
+        """ Open dialog and save filtered data to pickle-file.
+
+        Returns
+        -------
+        bool
+            True if progress was saved else False.
+        """
         to_save = dict()
         for i, key in enumerate(self.keys):
             if self.list_model.is_good[i]:
@@ -71,12 +109,21 @@ class TextPickerWidget(QtGui.QWidget):
             with open(fname, 'wb') as f:
                 pickle.dump(to_save, f)
 
+            self.has_saved = True
+            return True
+
+        return False
+
     def list_changed(self, selection):
         if selection.indexes():
-            self.text_window.clear()
             i = selection.indexes()[0].row()
-            self.current_selection = i
-            self.text_window.setText(self.data[self.keys[i]])
+            self.change_display(i)
+
+    def change_display(self, i):
+        self.text_window.clear()
+        self.current_selection = i
+        self.text_window.setText(self.data[self.keys[i]])
+        self.label.setText(self.base_string.format(i + 1))
 
     def set_selected(self, i):
         """
@@ -89,14 +136,12 @@ class TextPickerWidget(QtGui.QWidget):
         if not (0 <= i < self.list_model.rowCount()):
             return
 
-        self.current_selection = i
         qindex = self.list_model.createIndex(i, 0)
         sel_model = self.list_view.selectionModel()
         sel_model.clearSelection()
         sel_model.setCurrentIndex(qindex, QtGui.QItemSelectionModel.Select)
 
-        self.text_window.clear()
-        self.text_window.setText(self.data[self.keys[i]])
+        self.change_display(i)
 
     def keyPressEvent(self, event):
         """ On right-key press go to next.
@@ -120,9 +165,51 @@ class TextPickerWidget(QtGui.QWidget):
         self.set_selected(self.current_selection - 1)
 
     def toggle_bad(self):
+        self.has_saved = False
         is_good = self.list_model.is_good[self.current_selection]
         self.list_model.is_good[self.current_selection] = not is_good
         self.list_view.update()
+
+
+class PickKeyDialog(QtGui.QDialog):
+
+    def __init__(self, keys):
+        super(PickKeyDialog, self).__init__()
+        self.keys = keys
+        self.buttons = list()
+        self.button_group = None
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('Pick data to filter.')
+        layout = QtGui.QVBoxLayout()
+        for key in self.keys:
+            button = QtGui.QRadioButton(key)
+            button.toggled.connect(self.enable_ok)
+            self.buttons.append(button)
+            layout.addWidget(button)
+
+        self.button_group = QtGui.QDialogButtonBox(
+            QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel,
+            QtCore.Qt.Horizontal
+        )
+        self.button_group.button(QtGui.QDialogButtonBox.Ok).clicked.connect(
+            self.accept
+        )
+        self.button_group.button(QtGui.QDialogButtonBox.Cancel).clicked.connect(
+            self.reject
+        )
+        self.button_group.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
+        layout.addWidget(self.button_group)
+        self.setLayout(layout)
+
+    def get_values(self):
+        checked_button = next(b for b in self.buttons if b.isChecked())
+        return checked_button.text()
+
+    def enable_ok(self, toggled):
+        self.button_group.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
+
 
 
 if __name__ == '__main__':
@@ -137,7 +224,23 @@ if __name__ == '__main__':
     else:
         sys.exit()
 
-    window = TextPickerWidget(data)
-    window.show()
+    try:
+        pick_dialog = PickKeyDialog(data.keys())
+    except AttributeError:
+        QtGui.QMessageBox().critical(None, 'Error', 'Wrong format.')
+        sys.exit(1)
 
+    if pick_dialog.exec_():
+        key = pick_dialog.get_values()
+    else:
+        sys.exit()
+
+    try:
+        window = TextPickerWidget(data[key])
+    except AttributeError:
+        msg = 'Wrong format. Data must be key, value-pairs.'
+        QtGui.QMessageBox().critical(None, 'Error', msg)
+        sys.exit(1)
+
+    window.show()
     sys.exit(app.exec_())
